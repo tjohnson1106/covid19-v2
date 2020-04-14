1c596744c23c56a4d49e188afa1c5a91549cbc39
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2020-04-13 21:59:28 -0400

    added axios and use effect method to implement API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
+import axios from "axios";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [latest, setLatest] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://corona.lmao.ninja/all")
+      .then((res) => {
+        setLatest(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
       <CardDeck>
-        <Card bg="secondary" text="white">
+        <Card
+          bg="secondary"
+          text="white"
+          className="text-center"
+          style={{
+            margin: "10px",
+          }}
+        >
           <Card.Body>
             <Card.Title>Cases</Card.Title>
-            <Card.Text>1000</Card.Text>
+            <Card.Text>{latest.cases}</Card.Text>
           </Card.Body>
           <Card.Footer>
             <small>Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
-        <Card bg="danger" text={"white"} className="text-center">
+        <Card
+          bg="danger"
+          text={"white"}
+          className="text-center"
+          style={{
+            margin: "10px",
+          }}
+        >
           <Card.Body>
             <Card.Title>Deaths</Card.Title>
             <Card.Text>33</Card.Text>
@@ -26,7 +55,14 @@ function App() {
             <small>Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
-        <Card bg="success" text={"white"} className="text-center">
+        <Card
+          bg="success"
+          text={"white"}
+          className="text-center"
+          style={{
+            margin: "10px",
+          }}
+        >
           <Card.Body>
             <Card.Title>Recovered</Card.Title>
             <Card.Text>12</Card.Text>
